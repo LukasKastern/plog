c631b61f2158fd6d897a864707d7e09662226f53
Lukas Kastern <lukaskastern@web.de>
2023-07-01 13:54:27 +0200

    Add build.zig

---
+++ build.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build.Builder) void {
+    var optimize = b.standardOptimizeOption(.{});
+    var target = b.standardTargetOptions(.{});
+
+    var plog = b.addStaticLibrary(.{ .name = "plog", .target = target, .optimize = optimize });
+
+    plog.addCSourceFiles(
+        &[_][]const u8{},
+        &[_][]const u8{},
+    );
+
+    plog.addIncludePath("include/");
+    b.installArtifact(plog);
+}
